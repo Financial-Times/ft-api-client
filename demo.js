d15515bc353395bb9ed68808485d58ebbe76b955
richard.still <richard.still@ft.com>
2013-01-14 18:02:17 +0000

    P004 Refactored module for simplicity, added ability to fetch muliple pages and page's main content

--- demo.js
+++ demo.js
@@ -1,28 +1,27 @@
 /*global console:true*/
 var ftApi = require('./ftApi.js'),
-	apiKey = "XXXXXXX";
+	apiKey = "f65958a8e35bd14bc52f268b8b3ab4ad";
 
 // Fetch a list of the latest notifications form the CAPI
 function getNotifications () {
 	"use strict";
 	var config = {}, // Config to be used when the request is made
-		initConfig = {}, // Config to be used when the object is initialised
 		notificationsFetcher;
 
 
 	// Config can be set when a new GetChangesFromCapi object is created or when the request is made
 	// The 'apiKey' and the 'since' date are required.
-	initConfig.apiKey					= apiKey; // Required, your API key
+	config.apiKey					= apiKey; // Required, your API key
 	//initConfig.aggregateResponse		= true; Optional, set by true default: Combine all the responses into an array and return them when 'loadComplete' fires
 	//initConfig.apiDomain				= 'api.ft.com'; // Optional, set by default: The domain for the CAPI
 	//initConfig.itemNotificationsPath	= '/content/notifications/v1/items'; // Optional, set by default
 	//initConfig.apiUpdateDelay			= 125; // Optional, set by default: Time in ms between requests, used to control the speed of comms to the API
 	//config.limit						= 20; // Optional, set by default: The number of items returned per request
 
-	notificationsFetcher = new ftApi.notifications.GetChangesFromCapi(initConfig);
+	notificationsFetcher = ftApi.notifications;
 
 	// The since date is required, should be in ISO format
-	config.since = '2013-01-08T13:00:00z'; // Required
+	config.since = '2013-01-14T16:45:00z'; // Required
 
 	// Get a list of modified articles from the CAPI
 	notificationsFetcher.fetchItems(config);
@@ -52,7 +51,7 @@ function getNotifications () {
 	});
 
 }
-//getNotifications();
+getNotifications();
 
 
 function getApiData (itemsList) {
@@ -70,47 +69,28 @@ function getApiData (itemsList) {
 	//config.apiUpdateDelay			= 125; // Optional, set by default: Time in ms between requests, used to control the speed of comms to the API
 
 	// Create a new GetDataFromContentApi object and pass in any required config
-	dataFetcher = new ftApi.content.GetDataFromContentApi(config);
+	//dataFetcher = new ftApi.content.GetDataFromContentApi(config);
 
 	// Request the content from the API. Content is fetched synchronously and throttled using the apiUpdateDelay property of config.
 	// Pass an array of IDs
-	dataFetcher.getApiContent(itemsList);
+	//dataFetcher.getApiContent(itemsList);
+	ftApi.content.getApiContent(itemsList, config);
 
 	// An 'itemLoadComplete' event will fire after each item is successfully loaded
-	dataFetcher.on('itemLoadComplete', function (data) {
+	ftApi.content.on('itemLoaded', function (data) {
 		console.log('Individual request complete');
+		//console.log(data);
 	});
 
 	// A load complete event will fire when all content is loaded.
-	dataFetcher.on('loadComplete', function (responseData) {
+	ftApi.content.on('allItemsLoaded', function (responseData) {
 		// Returns a list of response CAPI response items
-		console.log(responseData);
+		console.log('All content loaded');
+		//console.log(responseData);
 	});
 }
 
 
-// Fetch a list of FT pages, then fetch the UK home page
-function getFtPages () {
-	"use strict";
-	var config = {}; // Config to be used when the request is made
-	
-	config.apiKey						= apiKey; // Required, your API key
-	//initConfig.apiDomain				= 'api.ft.com'; // Optional, set by default: The domain for the CAPI
-	//initConfig.pagePath				= '/site/v1/pages/'; // Optional, set by default
 
-	// Create a pages object
-	var ftPages = new ftApi.pages.GetPagesFromContentApi(config);
-	ftPages.getPages();
 
-	ftPages.on('pageListLoadComplete', function (pageList) {
-		console.log('Page list:', pageList);
-		//UK homepage 4c499f12-4e94-11de-8d4c-00144feabdc0
-		ftPages.getPage('4c499f12-4e94-11de-8d4c-00144feabdc0');
-	});
 
-	ftPages.on('pageLoadComplete', function (page) {
-		console.log('UK Homepage', page);
-	});
-
-}
-getFtPages();
--- ftApi.js
+++ ftApi.js
@@ -1,9 +1,50 @@
+//Imported FT modules
 var notificationsModule	= require('./lib/getContentItemsNotification.js'),
 	contentModule		= require('./lib/getContentApiContent.js'),
 	pagesModule			= require('./lib/getContentApiPages.js'),
 	apiUtils			= require('./lib/apiUtils.js');
 
-exports.notifications	= notificationsModule;
-exports.content			= contentModule;
+// Imported Node modules
+var	EventEmitter	= require('events').EventEmitter,
+	utils			= require('util');
+
+
+// Define the Class for fetching notifications from the CAPI
+var GetChangesFromCapi = function () {};
+
+// Inherit form the event emitter so we can emit custom events, useful for preserving sanity
+utils.inherits(GetChangesFromCapi, EventEmitter);
+
+// Default configuration data, only the 'apiKey' and 'since' do not have default values
+GetChangesFromCapi.prototype.config = notificationsModule.config;
+GetChangesFromCapi.prototype.fetchItems = notificationsModule.fetchItems;
+
+
+
+// Define a class for retrieving content items and pages from the CAPI
+GetDataFromContentApi = function (passedConfig) {};
+// Inherit form the event emitter so we can emit custom events, useful for preserving sanity
+utils.inherits(GetDataFromContentApi, EventEmitter);
+
+GetDataFromContentApi.prototype.getApiContent = contentModule.getApiContent;
+
+// Get an individual page from the CAPI
+GetDataFromContentApi.prototype.getPage = contentModule.getPage;
+
+// Get and individual page from the CAPI
+GetDataFromContentApi.prototype.getPageMainContent = contentModule.getPageMainContent;
+
+// Get a list pf pages from the CAPI
+GetDataFromContentApi.prototype.getPages = contentModule.getPages;
+
+// Default configuration data, only the 'apiKey' and 'since' do not have default values
+GetDataFromContentApi.prototype.config = contentModule.config;
+
+GetDataFromContentApi.prototype.getApiItem = contentModule.getApiItem;
+
+
+
+// Export the modules
+exports.notifications	= new GetChangesFromCapi();
+exports.content			= new GetDataFromContentApi();
 exports.utils			= apiUtils;
-exports.pages			= pagesModule;
