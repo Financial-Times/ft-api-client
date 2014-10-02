d83a4e89fd7c2bcbabcc4f74857c118fd7481467
Matt Chadburn <matt.chadburn@ft.com>
2014-10-02 18:49:29 +0100

    Find page API resource by title with mocha tests

--- models/pages.js
+++ models/pages.js
@@ -2,20 +2,10 @@
 var pageList = require('../data/pagesList.json');
 var _ = require('lodash');
 
-function getItemByTitle (title) {
+module.exports.findByTitle = function (title) {
 	return _.find(pageList.pages, function (item) {
 		if (item.title === title) {
 			return item;
 		}
 	});
 }
-
-function getApiUrlbyTitle (title) {
-	if(getItemByTitle(title)) {
-		return getItemByTitle(title).apiUrl;
-	} else {
-		return false;
-	}
-}
-
-module.exports.getItemByTitle = getApiUrlbyTitle;
