a69c0def17f2389190d0d40b8a2fcef0fa77eee8
Matt Chadburn <matt.chadburn@ft.com>
2014-10-17 21:34:46 +0100

    jsdoc

--- models/cheerio/find-inbound-links.js
+++ models/cheerio/find-inbound-links.js
@@ -2,9 +2,9 @@
 var cheerio = require('cheerio');
 var url     = require('url');
 
-// Fix any old ft links, Eg. 
-//  www.ft.com/cms/s/43515588-00fc-11e4-a938-00144feab7de.html -> /43515588-00fc-11e4-a938-00144feab7de 
-
+/**
+ * Returns a list of internal ft.com links from a given HTML string
+ */
 module.exports = function (html) {
     var $ = cheerio.load(html);
     return $('a').filter(function(i, el) {
