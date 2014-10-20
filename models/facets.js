440fec223b69c4d42478dc78a59a595f5dd87e99
Paul I Wilson <paul.i.wilson@ft.com>
2014-10-20 11:45:45 +0100

    Fixing stupid bug in parsing facets data

--- models/facets.js
+++ models/facets.js
@@ -1,8 +1,7 @@
 
 function Facets(raw){
     this.name = raw.name;
-    this.elements = [];
-    var elements = raw.facetElements;
+    this.elements = raw.facetElements;
 
     this.elements.sort(function(a,b){
         return b.count - a.count;
